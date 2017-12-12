e8cfa9c55b46b56f502205b494c343c97e096ca9
shivapbhusal <civabhusal@gmail.com>
2017-12-11 19:42:13 -0500

    add source files

---
+++ Calculator.hpp
@@ -0,0 +1,13 @@
+/* Main file for calculator.h
+*/
+
+#include<iostream>
+#include"Calculator.hpp"
+
+int Calculator::sum(int a, int b){
+  		return a+b; 
+}
+
+int Calculator::subs(int a, int b){
+  		return a-b; 
+} 
